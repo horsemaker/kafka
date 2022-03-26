d0bd6e66a72eab54e2430cf30e838fe887552ae0
horsemaker <yash_ghodekar@yahoo.in>
2022-03-26 17:38:11 +0530

    add home screen assets constants

--- src/frontend/constants/assets-constants.js
+++ src/frontend/constants/assets-constants.js
@@ -1,2 +1,7 @@
 export const LOGO_DARK = "/assets/Kafka/Logos/kafka-logo-dark.svg";
 export const LOGO_LIGHT = "/assets/Kafka/Logos/kafka-logo-light.svg";
+
+export const HOME_SCREEN_IMG_DARK =
+  "/assets/Kafka/Storyset/HomeScreen/home-screen-img-dark.svg";
+export const HOME_SCREEN_IMG_LIGHT =
+  "/assets/Kafka/Storyset/HomeScreen/home-screen-img-light.svg";
