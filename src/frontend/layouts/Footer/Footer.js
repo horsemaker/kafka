32f4246cef13889fb48f4b23f47ea7e8e1d07498
horsemaker <yash_ghodekar@yahoo.in>
2022-03-26 13:45:33 +0530

    fix: footer icon links

--- src/frontend/layouts/Footer/Footer.js
+++ src/frontend/layouts/Footer/Footer.js
@@ -5,16 +5,28 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <section className="footer-icons">
-        <a href="https://www.instagram.com/horsemaker_/">
+        <a
+          href="https://www.instagram.com/horsemaker_/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <i className="fa-brands fa-instagram footer-icon"></i>
         </a>
-        <a href="https://www.linkedin.com/in/yashghodekar/">
+        <a
+          href="https://www.linkedin.com/in/yashghodekar/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <i className="fa-brands fa-linkedin-in footer-icon"></i>
         </a>
-        <a href="https://www.doglapan.com/">
+        <a href="https://www.doglapan.com/" target="_blank" rel="noreferrer">
           <i className="fa-brands fa-facebook-f footer-icon"></i>
         </a>
-        <a href="https://github.com/horsemaker/oasis">
+        <a
+          href="https://github.com/horsemaker/kafka"
+          target="_blank"
+          rel="noreferrer"
+        >
           <i className="fa-brands fa-github footer-icon"></i>
         </a>
       </section>
