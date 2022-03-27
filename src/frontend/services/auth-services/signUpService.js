632cfb73986e9ae6155e9dfb6e30debb47fac2be
horsemaker <yash_ghodekar@yahoo.in>
2022-03-27 19:38:56 +0530

    add auth services

---
+++ src/frontend/services/auth-services/signUpService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const signUpService = async (user) => {
+  try {
+    const { data } = await axios.post("/api/auth/signup", user);
+    console.log(data);
+    return {
+      userToken: data.encodedToken,
+      userData: {
+        firstName: data.createdUser?.firstName,
+        lastName: data.createdUser?.lastName,
+        email: data.createdUser?.email,
+      },
+    };
+  } catch (error) {
+    console.error(error.message);
+    return;
+  }
+};
