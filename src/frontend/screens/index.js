1aababa3cbd9f5d42ce8bff571ab9f6b2625c6e7
horsemaker <yash_ghodekar@yahoo.in>
2022-03-30 21:12:29 +0530

    add notes screen

--- src/frontend/screens/index.js
+++ src/frontend/screens/index.js
@@ -1,3 +1,4 @@
 export { HomeScreen } from "./HomeScreen/HomeScreen";
 export { SignInScreen } from "./SignInScreen/SignInScreen";
 export { SignUpScreen } from "./SignUpScreen/SignUpScreen";
+export { NotesScreen } from "./NotesScreen/NotesScreen";
