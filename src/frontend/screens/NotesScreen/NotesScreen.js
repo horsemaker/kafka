5a3fee237edf3735a9b2393dc2efd758bd325999
horsemaker <yash_ghodekar@yahoo.in>
2022-04-01 17:45:42 +0530

    `h4` => `h6`

--- src/frontend/screens/NotesScreen/NotesScreen.js
+++ src/frontend/screens/NotesScreen/NotesScreen.js
@@ -12,13 +12,13 @@ export const NotesScreen = () => {
       <div className="notes">
         {pinnedNotes.length !== 0 && (
           <div className="notes-pinned notes-display">
-            {otherNotes.length !== 0 && <h4>Pinned</h4>}
+            {otherNotes.length !== 0 && <h6>Pinned</h6>}
             <NotesListing notes={pinnedNotes} />
           </div>
         )}
         {otherNotes.length !== 0 && (
           <div className="notes-other notes-display">
-            {pinnedNotes.length !== 0 && <h4>Others</h4>}
+            {pinnedNotes.length !== 0 && <h6>Others</h6>}
             <NotesListing notes={otherNotes} />
           </div>
         )}
