deb9adb978cb88a6406595780cf7a4fc3fe112c5
horsemaker <yash_ghodekar@yahoo.in>
2022-04-06 01:50:24 +0530

    refactor: capitalize

--- src/frontend/screens/NotesScreen/NotesScreen.js
+++ src/frontend/screens/NotesScreen/NotesScreen.js
@@ -23,13 +23,13 @@ export const NotesScreen = () => {
       <div className="notes">
         {pinnedNotes.length !== 0 && (
           <div className="notes-pinned notes-display">
-            {otherNotes.length !== 0 && <h6>Pinned</h6>}
+            {otherNotes.length !== 0 && <h6>PINNED</h6>}
             <NotesListing notes={pinnedNotes} />
           </div>
         )}
         {otherNotes.length !== 0 && (
           <div className="notes-other notes-display">
-            {pinnedNotes.length !== 0 && <h6>Others</h6>}
+            {pinnedNotes.length !== 0 && <h6>OTHERS</h6>}
             <NotesListing notes={otherNotes} />
           </div>
         )}
